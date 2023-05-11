624d1c5611ba98cda8e1f171f3ac099d4268745a
Raul <raulelwork@gmail.com>
2023-05-11 16:03:06 +0200

    funciona el alert al reservar

--- resources/js/Pages/entradas.vue
+++ resources/js/Pages/entradas.vue
@@ -11,6 +11,8 @@ import { Link } from "@inertiajs/vue3";
 import NavLink from "@/components/NavLink.vue";
 // import required modules
 import Layout from '@/components/Layout.vue';
+import Swal from 'sweetalert2';
+
 
 export default {
     components: {
@@ -18,15 +20,24 @@ export default {
         SwiperSlide,
         Layout,
         NavLink,
-        Link
+        Link,
     },
     data() {
         return {
             showMenu: false,
             fiestas: [],
             empresas: [],
+            color: ['text-blue-600', 'text-pink-600', 'text-red-600', 'text-green-600', 'text-purple-600', 'text-violet-600'],
         };
     }, methods: {
+        showAlert() {
+            Swal.fire({
+                title: 'Reserva Realizada!',
+                text: 'Puedes visualizar la reserva en MIS RESERVAS.',
+                icon: 'success',
+                confirmButtonColor: '#1a202c'
+            });
+        },
         formatdate(dateTimeString) {
             return this.formatearFecha(dateTimeString.slice(0, 10));
         },
@@ -37,6 +48,11 @@ export default {
             const anio = dateObj.getFullYear().toString();
             return `${dia}-${mes}-${anio}`;
         },
+        orderByDate: function (fiestas) {
+            return fiestas.sort(function (a, b) {
+                return new Date(a.fecha) - new Date(b.fecha);
+            });
+        }
     },
     setup() {
         return {
@@ -61,7 +77,9 @@ export default {
             });
     },
 
+
 };
+
 </script>
 
 <template>
@@ -82,11 +100,12 @@ export default {
     clickable: true,
 }" :navigation="false" :modules="modules" class="mySwiper">
                     <swiper-slide class="mySwiper" v-for="empresa in empresas" :key="empresa.id">
-                        <nav-link :href="'perfil/'+empresa.id">
+                        <nav-link :href="'perfil/' + empresa.id">
                             <div
                                 class="bg-black/60 max-w-sm rounded-sm overflow-hidden shadow-lg  m-7 p-4 transition duration-500 hover:scale-110">
                                 <h2 class="text-white text-4xl m-3 text-center">{{ empresa.nombre }}</h2>
-                                <img v-bind:src="'http://[::1]:5173/storage/empresas/' + empresa.foto" class="mb-4 object-cover h-72 w-72 " alt="">
+                                <img v-bind:src="'http://[::1]:5173/storage/empresas/' + empresa.foto"
+                                    class="mb-4 object-cover h-72 w-72 " alt="">
                             </div>
                         </nav-link>
                     </swiper-slide>
@@ -96,7 +115,7 @@ export default {
             <div class=" min-[450px]:grid place-items-center  border-t-4 border-t-black" id="entradas">
                 <div class=" flex flex-wrap justify-center min-[1400px]:grid min-[1400px]:grid-cols-4">
                     <div class="bg-white/90 max-w-sm rounded-t-xl overflow-hidden shadow-lg  m-7  max-[450px]:scale-75"
-                        v-for="fiesta in fiestas" :key="fiesta.id">
+                        v-for="fiesta in  orderByDate(fiestas)" :key="fiesta.id">
                         <img v-bind:src="'http://[::1]:5173/storage/fiestas/' + fiesta.foto"
                             class="mb-4 w-full h-80 object-cover" alt="">
                         <h1 class="text-lg text-center">{{ fiesta.empresa.nombre }} -> {{ formatdate(fiesta.fecha) }}</h1>
@@ -108,7 +127,10 @@ export default {
                         </div>
                         <div class="m-2">
                             <h1 class="text-base text-center">ENTRADAS</h1>
-                            <p class="flex justify-center items-center mt-3" v-if="!$page.props.auth.user">Se requiere  <nav-link class="hover:text-blue-500 no-underline hover:scale-105 text-pink-600 mx-3" href="/login">Inicia Sesion</nav-link> para reservar</p>
+                            <p class="flex justify-center items-center mt-3" v-if="!$page.props.auth.user">Se requiere
+                                <nav-link class="hover:text-blue-500 no-underline hover:scale-105 text-pink-600 mx-3"
+                                    href="/login">Inicia Sesion</nav-link> para reservar
+                            </p>
                             <swiper :spaceBetween="30" :centeredSlides="true" :autoplay="{
                                 delay: 4500,
                                 disableOnInteraction: false,
@@ -117,12 +139,12 @@ export default {
 
                                 <swiper-slide v-for="e in fiesta.entrada"
                                     class="mySwiper max-[400px]:flex max-[400px]:flex-col" :key="e.id">
-                                    <p class="m-2 text-pink-600 text-lg">{{ e.tipo }}</p>
+                                    <p :class="color[Math.floor(Math.random() * 6)] + ' text-lg'">{{ e.tipo }}</p>
                                     <p class="m-2"> {{ e.consumiciones }} Copas </p>
                                     <p class="m-2"> {{ e.precio }}€ </p>
                                     <div
                                         class="float-right px-3 m-2 border-2 rounded-xl border-blue-600 hover:bg-blue-700/80 duration-300 hover:scale-105 text-white bg-blue-500/80 text-lg justify-end ">
-                                        <button>Reservar</button>
+                                        <button @click="showAlert">Reservar</button>
                                     </div>
                                 </swiper-slide>
                             </swiper>
